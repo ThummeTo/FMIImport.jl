23b82cf62151785ade4a82ea2e9ce977a6ee82cb
JoKircher <josef_kircher@gmx.de>
2022-04-04 15:38:25 -0230

    modeldescription passing tests

--- test/FMI3/model_description.jl
+++ test/FMI3/model_description.jl
@@ -3,29 +3,50 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
-using FMIImport.FMICore: fmi2VariableNamingConventionStructured
+using FMIImport.FMICore: fmi3VariableNamingConventionFlat
+using FMIImport
+using ZipFile
 
-myFMU = fmi3Load("SpringFrictionPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
+zipPath = download("https://github.com/modelica/Reference-FMUs/releases/download/v0.0.14/Reference-FMUs-0.0.14.zip")
+dir = dirname(zipPath)
+zipPath = normpath(zipPath)
+zarchive = ZipFile.Reader(zipPath)
+path = joinpath(dir, "BouncingBall/")
+pathToFmu = joinpath(path, "BouncingBall.fmu")
+for f in zarchive.files
+    if f.name == "3.0/BouncingBall.fmu"
+        if !ispath(path)
+            mkdir(path)
+        end
+      
+        numBytes = write(pathToFmu, read(f))
+        if numBytes == 0
+            print("Not able to read!")
+        end
+    end
+end
+close(zarchive)
+myFMU = fmi3Load(pathToFmu, ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
 
-@test fmi3GetVersion(myFMU) == "2.0"
-@test fmi3GetTypesPlatform(myFMU) == "default"
+@test fmi3GetVersion(myFMU) == "3.0-beta.5"
 
-@test fmi3GetModelName(myFMU) == "SpringFrictionPendulum1D"
-@test fmi3GetVariableNamingConvention(myFMU) == fmi2VariableNamingConventionStructured
+@test fmi3GetModelName(myFMU) == "BouncingBall"
+@test fmi3GetVariableNamingConvention(myFMU) == fmi3VariableNamingConventionFlat
 @test fmi3IsCoSimulation(myFMU) == true
 @test fmi3IsModelExchange(myFMU) == true
 # TODO scheduledExecution
-@test fmi3GetInstantiationToken(myFMU) == "{df491d8d-0598-4495-913e-5b025e54d7f2}" # TODO update
-@test fmi3GetGenerationTool(myFMU) == "Dymola Version 2022x (64-bit), 2021-10-08"
-@test fmi3GetGenerationDateAndTime(myFMU) == "2022-03-03T15:09:18Z"
-@test fmi3GetNumberOfEventIndicators(myFMU) == 24
+@test fmi3GetInstantiationToken(myFMU) == "{8c4e810f-3df3-4a00-8276-176fa3c9f003}" # TODO update
+@test fmi3GetGenerationTool(myFMU) == "Reference FMUs (0278b1e)"
+@test fmi3GetGenerationDateAndTime(myFMU) == "[Unknown generation date and time]"
+@test fmi3GetNumberOfEventIndicators(myFMU) == 1
 @test fmi3CanGetSetState(myFMU) == true
-@test fmi3CanSerializeFMUstate(myFMU) == true
-@test fmi3ProvidesDirectionalDerivative(myFMU) == true
+@test fmi3CanSerializeFMUState(myFMU) == true
+@test fmi3ProvidesDirectionalDerivatives(myFMU) == false
+@test fmi3ProvidesAdjointDerivatives(myFMU) == false
 
 @test fmi3GetDefaultStartTime(myFMU.modelDescription) ≈ 0.0
-@test fmi3GetDefaultStopTime(myFMU.modelDescription) ≈ 1.0
+@test fmi3GetDefaultStopTime(myFMU.modelDescription) ≈ 3.0
 @test fmi3GetDefaultTolerance(myFMU.modelDescription) ≈ 1e-4
-@test fmi3GetDefaultStepSize(myFMU.modelDescription) === nothing
+@test fmi3GetDefaultStepSize(myFMU.modelDescription) === 0.01
 
 fmi3Unload(myFMU)
