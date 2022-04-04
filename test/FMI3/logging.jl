c73001b67c92cadcaf227b2eabcc06a4b9bfa0ed
JoKircher <josef_kircher@gmx.de>
2022-04-04 16:00:12 -0230

    crashes might needed fixing later

--- test/FMI3/logging.jl
+++ test/FMI3/logging.jl
@@ -5,8 +5,33 @@
 
 import FMIImport: fmi3StatusError
 
-myFMU = fmi3Load("SpringPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
+using FMIImport
+using ZipFile
 
+zipPath = download("https://github.com/modelica/Reference-FMUs/releases/download/v0.0.14/Reference-FMUs-0.0.14.zip")
+# path = joinpath(test)
+dir = dirname(zipPath)
+zipPath = normpath(zipPath)
+zarchive = ZipFile.Reader(zipPath)
+path = joinpath(dir, "BouncingBall/")
+pathToFmu = joinpath(path, "BouncingBall.fmu")
+for f in zarchive.files
+    #println(f.name)
+    if f.name == "3.0/BouncingBall.fmu"
+        if !ispath(path)
+            mkdir(path)
+        end
+        
+        numBytes = write(pathToFmu, read(f))
+        if numBytes == 0
+            print("Not able to read!")
+        end
+    end
+end
+close(zarchive)
+
+# myFMU = fmi3Load(pathToFmu, ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
+myFMU = fmi3Load(pathToFmu)
 ### CASE A: Print log ###
 inst = fmi3InstantiateCoSimulation!(myFMU; loggingOn=true)
 @test inst != 0
