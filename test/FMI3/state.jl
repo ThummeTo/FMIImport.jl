6fe6c7743ce7b3d78b8301799376e4f6832a9f9b
JoKircher <josef_kircher@gmx.de>
2022-04-04 13:05:28 -0230

    state finished and run successfully

--- test/FMI3/state.jl
+++ test/FMI3/state.jl
@@ -8,9 +8,30 @@
 ###############
 
 using FMIImport.FMICore: fmi3FMUState
+using FMIImport
+using ZipFile
 
-myFMU = fmi3Load("SpringPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
+zipPath = download("https://github.com/modelica/Reference-FMUs/releases/download/v0.0.14/Reference-FMUs-0.0.14.zip")
+dir = dirname(zipPath)
+zipPath = normpath(zipPath)
+zarchive = ZipFile.Reader(zipPath)
+path = joinpath(dir, "BouncingBall/")
+pathToFmu = joinpath(path, "BouncingBall.fmu")
+for f in zarchive.files
+    if f.name == "3.0/BouncingBall.fmu"
+        if !ispath(path)
+            mkdir(path)
+        end
+      
+        numBytes = write(pathToFmu, read(f))
+        if numBytes == 0
+            print("Not able to read!")
+        end
+    end
+end
+close(zarchive)
 
+myFMU = fmi3Load(pathToFmu, ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
 inst = fmi3InstantiateCoSimulation!(myFMU; loggingOn=true)
 @test inst != 0
 
@@ -21,28 +42,28 @@ inst = fmi3InstantiateCoSimulation!(myFMU; loggingOn=true)
 # Testing state functions #
 ###########################
 
-if fmi3CanGetSetState(myFMU) && fmi3CanSerializeFMUstate(myFMU)
-    @test fmi3GetFloat64(inst, "mass.s") == 0.5
+if fmi3CanGetSetState(myFMU) && fmi3CanSerializeFMUState(myFMU)
+    @test fmi3GetFloat64(inst, "h") == 1.0
     FMUState = fmi3GetFMUState(inst)
     @test typeof(FMUState) == fmi3FMUState
     len = fmi3SerializedFMUStateSize(inst, FMUState)
     @test len > 0
     serial = fmi3SerializeFMUState(inst, FMUState)
     @test length(serial) == len
-    @test typeof(serial) == Array{Char,1}
+    @test typeof(serial) == Array{Cuchar,1}
 
-    fmi3SetFloat64(inst, "mass.s", 10.0)
+    fmi3SetFloat64(inst, "h", 10.0)
     FMUState = fmi3GetFMUState(inst)
-    @test fmi3GetFloat64(inst, "mass.s") == 10.0
+    @test fmi3GetFloat64(inst, "h") == 10.0
 
     FMUState2 = fmi3DeSerializeFMUState(inst, serial)
     @test typeof(FMUState2) == fmi3FMUState
     fmi3SetFMUState(inst, FMUState2)
-    @test fmi3GetFloat64(inst, "mass.s") == 0.5
+    @test fmi3GetFloat64(inst, "h") == 1.0
     fmi3SetFMUState(inst, FMUState)
-    @test fmi3GetFloat64(inst, "mass.s") == 10.0
-    fmi3FreeFMUstate!(inst, FMUState)
-    fmi3FreeFMUstate!(inst, FMUState2)
+    @test fmi3GetFloat64(inst, "h") == 10.0
+    fmi3FreeFMUState!(inst, FMUState)
+    fmi3FreeFMUState!(inst, FMUState2)
 else
     @info "The FMU provided from the tool `$(ENV["EXPORTINGTOOL"])` does not support state get, set, serialization and deserialization. Skipping related tests."
 end
