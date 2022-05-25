1351484402e31416452ec8e08da4cf1b9a2fec55
TT <tobias.thummerer@informatik.uni-augsburg.de>
2022-05-25 10:44:37 +0200

    typo

--- src/FMI2_ext.jl
+++ src/FMI2_ext.jl
@@ -734,5 +734,5 @@ Returns the `inital` entry of the corresponding model variable.
 ToDo: update docstring format.
 """
 function fmi2GetInitial(mv::fmi2ScalarVariable)
-    return mv..inital
+    return mv.inital
 end 
