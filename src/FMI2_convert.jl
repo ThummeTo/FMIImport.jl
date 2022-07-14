b5f5cd2656d504bcfbf39fd0a980086a6bb5b09f
Adrian Brune <adrian.brune@uni-a.de>
2022-07-14 14:22:16 +0200

    Updated FMI2_convert.jl

--- src/FMI2_convert.jl
+++ src/FMI2_convert.jl
@@ -23,7 +23,7 @@ function prepareValueReference(md::fmi2ModelDescription, vr::fmi2ValueReferenceF
     elseif tvr == Nothing
         return Array{fmi2ValueReference,1}()
     elseif tvr == Symbol
-        if vr == :states 
+        if vr == :states
             return md.stateValueReferences
         elseif vr == :derivatives
             return md.derivativeValueReferences
@@ -50,7 +50,22 @@ function prepareValueReference(comp::FMU2Component, vr::fmi2ValueReferenceFormat
 end
 
 """
+
+   fmi2StringToValueReference(md::fmi2ModelDescription, names::AbstractArray{String})
+
 Returns an array of ValueReferences coresponding to the variable names.
+
+# Arguments
+- `md::fmi2ModelDescription`: Argument `md` stores all static information related to an FMU. Especially, the FMU variables and their attributes such as name, unit, default initial value, etc..
+- `names::AbstractArray{String}`: Argument `names` contains a list of Strings. For each string ("variable name"), the corresponding value reference is searched in the given modelDescription.
+
+# Returns
+- `vr:Array{fmi2ValueReference}`: Return `vr` is an array of `ValueReference` coresponding to the variable names.
+
+# Source
+- FMISpec2.0.2 Link: [https://fmi-standard.org/](https://fmi-standard.org/)
+- FMISpec2.0.2[p.16]: 2.1.2 Platform Dependent Definitions
+See also [`fmi2StringToValueReference`](@ref)
 """
 function fmi2StringToValueReference(md::fmi2ModelDescription, names::AbstractArray{String})
     vr = Array{fmi2ValueReference}(undef,0)
@@ -65,21 +80,50 @@ function fmi2StringToValueReference(md::fmi2ModelDescription, names::AbstractArr
     vr
 end
 
-""" 
+"""
+
+   fmi2ModelVariablesForValueReference(md::fmi2ModelDescription, vr::fmi2ValueReference)
+
 Returns the model variable(s) fitting the value reference.
+
+# Arguments
+- `md::fmi2ModelDescription`: Argument `md` stores all static information related to an FMU. Especially, the FMU variables and their attributes such as name, unit, default initial value, etc..
+- `vr::fmi2ValueReference`: Argument `vr` contains a value of type`fmi2ValueReference` which are identifiers of a variable value of the model.
+
+# Returns
+- `ar::Array{fmi2ScalarVariable}`: Return `ar` is an array of `fmi2ScalarVariable` containing the modelVariables with the identical fmi2ValueReference to the input variable vr.
+
+# Source
+- FMISpec2.0.2 Link: [https://fmi-standard.org/](https://fmi-standard.org/)
+- FMISpec2.0.2[p.16]: 2.1.2 Platform Dependent Definitions
+See also [`fmi2ModelVariablesForValueReference`](@ref)
 """
 function fmi2ModelVariablesForValueReference(md::fmi2ModelDescription, vr::fmi2ValueReference)
     ar = []
     for modelVariable in md.modelVariables
-        if modelVariable.valueReference == vr 
+        if modelVariable.valueReference == vr
             push!(ar, modelVariable)
-        end 
-    end 
+        end
+    end
     ar
 end
 
 """
+   fmi2StringToValueReference(md::fmi2ModelDescription, name::String)
+
 Returns the ValueReference coresponding to the variable name.
+
+# Arguments
+- `md::fmi2ModelDescription`: Argument `md` stores all static information related to an FMU. Especially, the FMU variables and their attributes such as name, unit, default initial value, etc..
+- `name::String`: Argument `names` contains a String or a list of Strings. For each string ("variable name"), the corresponding value reference is searched in the given modelDescription.
+
+# Returns
+- `reference::md.stringValueReferences`: Return `references` is an array of `ValueReference` coresponding to the variable name.
+
+# Source
+- FMISpec2.0.2 Link: [https://fmi-standard.org/](https://fmi-standard.org/)
+- FMISpec2.0.2[p.16]: 2.1.2 Platform Dependent Definitions
+See also [`fmi2StringToValueReference`](@ref)
 """
 function fmi2StringToValueReference(md::fmi2ModelDescription, name::String)
     reference = nothing
@@ -91,12 +135,43 @@ function fmi2StringToValueReference(md::fmi2ModelDescription, name::String)
     reference
 end
 
+"""
+
+   fmi2StringToValueReference(fmu::FMU2, name::Union{String, AbstractArray{String}})
+
+Returns the ValueReference or an array of ValueReferences coresponding to the variable names.
+
+# Arguments
+- `md::fmi2ModelDescription`: Argument `md` stores all static information related to an FMU. Especially, the FMU variables and their attributes such as name, unit, default initial value, etc..
+- `name::Union{String, AbstractArray{String}}`: Argument `names` contains a Strings or . For that, the corresponding value reference is searched in the given modelDescription.
+
+# Returns
+For input parameter `name::Sting`:
+- `reference::md.stringValueReferences`: Return `references` is an array of `ValueReference` coresponding to the variable name.
+For input parameter `name::AbstractArray{String}`
+- `ar::Array{fmi2ScalarVariable}`: Return `ar` is an array of `fmi2ScalarVariable` containing the modelVariables with the identical fmi2ValueReference to the input variable vr.
+# Source
+- FMISpec2.0.2 Link: [https://fmi-standard.org/](https://fmi-standard.org/)
+- FMISpec2.0.2[p.16]: 2.1.2 Platform Dependent Definitions
+See also [`fmi2StringToValueReference`](@ref)
+"""
 function fmi2StringToValueReference(fmu::FMU2, name::Union{String, AbstractArray{String}})
     fmi2StringToValueReference(fmu.modelDescription, name)
 end
 
 """
-Returns an array of variable names matching a fmi2ValueReference.
+
+   fmi2ValueReferenceToString(md::fmi2ModelDescription, reference::fmi2ValueReference)
+
+???
+
+# Arguments
+- `md::fmi2ModelDescription`: Argument `md` stores all static information related to an FMU. Especially, the FMU variables and their attributes such as name, unit, default initial value, etc..
+- `reference::fmi2ValueReference`: Argument `references` is a variable of `ValueReference`.
+
+# Return
+- `::Bool`
+
 """
 function fmi2ValueReferenceToString(md::fmi2ModelDescription, reference::fmi2ValueReference)
     [k for (k,v) in md.stringValueReferences if v == reference]
@@ -109,81 +184,129 @@ function fmi2ValueReferenceToString(fmu::FMU2, reference::Union{fmi2ValueReferen
     fmi2ValueReferenceToString(fmu.modelDescription, reference)
 end
 
+
+"""
+
+   fmi2GetSolutionState(solution::FMU2Solution, vr::fmi2ValueReferenceFormat; isIndex::Bool=false)
+
+???
+
+# Arguments
+- `solution::FMU2Solution`:
+- `vr::fmi2ValueReferenceFormat`:
+- `isIndex::Bool=false`:
+
+# Return
+-`nothing`
+
+"""
+
 function fmi2GetSolutionState(solution::FMU2Solution, vr::fmi2ValueReferenceFormat; isIndex::Bool=false)
- 
+
     index = 0
 
     if isIndex
-        index = vr 
-    else 
+        index = vr
+    else
         ignore_derivatives() do
             vr = prepareValueReference(solution.fmu, vr)[1]
-        
+
             if solution.states !== nothing
                 for i in 1:length(solution.fmu.modelDescription.stateValueReferences)
                     if solution.fmu.modelDescription.stateValueReferences[i] == vr
-                        index = i 
-                        break 
+                        index = i
+                        break
                     end
                 end
             end
-           
+
         end # ignore_derivatives
     end
 
-    if index > 0 
+    if index > 0
         return collect(u[index] for u in solution.states.u)
     end
 
     return nothing
 end
 
+"""
+
+   fmi2GetSolutionValue(solution::FMU2Solution, vr::fmi2ValueReferenceFormat; isIndex::Bool=false)
+
+If `isIndex = false` the function returns an array which contains the solution states till the spezific solution element ("index") that equals the given fmi2ValueReferenceFormat.
+If `isIndex = true`
+# Arguments
+- `solution::FMU2Solution`:
+- `vr::fmi2ValueReferenceFormat`:
+- `isIndex::Bool=false`:
+
+# Return
+- `::Array{}`
+-`nothing`: If no
+
+"""
+
 function fmi2GetSolutionValue(solution::FMU2Solution, vr::fmi2ValueReferenceFormat; isIndex::Bool=false)
 
     index = 0
 
     if isIndex
-        index = vr 
-    else 
+        index = vr
+    else
         ignore_derivatives() do
             vr = prepareValueReference(solution.fmu, vr)[1]
-        
+
             if solution.states !== nothing
                 for i in 1:length(solution.fmu.modelDescription.stateValueReferences)
                     if solution.fmu.modelDescription.stateValueReferences[i] == vr
-                        index = i 
-                        break 
+                        index = i
+                        break
                     end
                 end
             end
 
-            if index > 0 
+            if index > 0
                 return collect(u[index] for u in solution.states.u)
             end
-        
+
             if solution.values !== nothing
                 for i in 1:length(solution.valueReferences)
                     if solution.valueReferences[i] == vr
-                        index = i 
-                        break 
+                        index = i
+                        break
                     end
                 end
             end
-           
+
         end # ignore_derivatives
     end
 
-    if index > 0 
+    if index > 0
         return collect(v[index] for v in solution.values.saveval)
     end
 
     return nothing
 end
 
+"""
+
+   fmi2GetSolutionTime(solution::FMU2Solution)
+
+???
+
+# Arguments
+- `solution::FMU2Solution`:
+
+# Return
+-`nothing`
+
+"""
+
 function fmi2GetSolutionTime(solution::FMU2Solution)
-    if solution.states !== nothing 
+    if solution.states !== nothing
         return solution.states.t
-    elseif solution.values !== nothing 
+    elseif solution.values !== nothing
         return solution.values.t
     else
         return nothing
