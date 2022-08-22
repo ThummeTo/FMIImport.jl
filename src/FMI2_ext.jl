8c73e47c18c8e1c6d50121430d751c21695da7e0
adribrune <adrianb.brune@gmail.com>
2022-08-22 09:27:55 +0200

    Updated FMI2_ext.jl

--- src/FMI2_ext.jl
+++ src/FMI2_ext.jl
@@ -878,7 +878,7 @@ end
 Returns the start/default value for a given value reference.
 
 # Arguments
-- `md::fmi2ModelDescription`: Struct wich provides the static information of ModelVariables.
+- `md::fmi2ModelDescription`: Struct which provides the static information of ModelVariables.
 - `vrs::fmi2ValueReferenceFormat = md.valueReferences`: wildcards for how a user can pass a fmi[X]ValueReference (default = md.valueReferences)
 More detailed: `fmi2ValueReferenceFormat = Union{Nothing, String, Array{String,1}, fmi2ValueReference, Array{fmi2ValueReference,1}, Int64, Array{Int64,1}, Symbol}`
 
