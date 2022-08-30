ebaf985b30387c9cced2c6d1f7dd0a8a6b55ddef
adribrune <adrianb.brune@gmail.com>
2022-08-30 07:42:27 +0200

    Update Doc-String FMI2_c.jl

--- src/FMI2_c.jl
+++ src/FMI2_c.jl
@@ -142,7 +142,7 @@ end
 - `c::FMU2Component`: Mutable struct represents an instantiated instance of an FMU in the FMI 2.0.2 Standard.
 # Returns
 - Returns a string from the address of a C-style (NUL-terminated) string. The string represents the version of the “fmi2Functions.h” header file which was used to compile the functions of the FMU. The function returns “fmiVersion” which is defined in this header file. The standard header file as documented in this specification has version “2.0”
-Source: FMISpec2.0.2[p.22]: 2.1.4 Inquire Platform and Version Number of Header Files
+
 
 # Source
 - FMISpec2.0.2 Link: [https://fmi-standard.org/](https://fmi-standard.org/)
