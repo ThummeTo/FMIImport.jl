4f07b951d4e1aeb1a04af87670f63e31f9592584
JoKircher <josef_kircher@gmx.de>
2022-10-10 11:00:30 +0200

    FMI3_fmu_to_md.jl fmi2 structure

--- src/FMI3_fmu_to_md.jl
+++ src/FMI3_fmu_to_md.jl
@@ -1,5 +1,5 @@
 # STATUS: check why GetEventIndicators got changed, lost during break
-
+# ABM: done
 
 #
 # Copyright (c) 2021 Tobias Thummerer, Lars Mikelsons, Josef Kircher
@@ -9,54 +9,101 @@
 # What is included in the file `FMI3_fmu_to_md.jl` (FMU to model description)?
 # - wrappers to call the model description functions from a FMU-instance [exported]
 
+"""
+TODO
+"""
+function fmi3GetNumberOfStates(fmu::FMU3)
+    fmi3GetNumberOfStates(fmu.modelDescription)
+end
+
+"""
+TODO
+"""
 function fmi3GetModelName(fmu::FMU3)
     fmi3GetModelName(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3GetInstantiationToken(fmu::FMU3)
     fmi3GetInstantiationToken(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3GetGenerationTool(fmu::FMU3)
     fmi3GetGenerationTool(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3GetGenerationDateAndTime(fmu::FMU3)
     fmi3GetGenerationDateAndTime(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3GetVariableNamingConvention(fmu::FMU3)
     fmi3GetVariableNamingConvention(fmu.modelDescription)
 end
+
 #TODO check if MD ending is needed in FMI.jl
+"""
+TODO
+"""
 function fmi3GetNumberOfEventIndicators(fmu::FMU3)
     fmi3GetNumberOfEventIndicators(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3CanGetSetState(fmu::FMU3)
     fmi3CanGetSetState(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3CanSerializeFMUState(fmu::FMU3)
     fmi3CanSerializeFMUState(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3ProvidesDirectionalDerivatives(fmu::FMU3)
     fmi3ProvidesDirectionalDerivatives(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3ProvidesAdjointDerivatives(fmu::FMU3)
     fmi3ProvidesAdjointDerivatives(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3IsCoSimulation(fmu::FMU3)
     fmi3IsCoSimulation(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3IsModelExchange(fmu::FMU3)
     fmi3IsModelExchange(fmu.modelDescription)
 end
 
+"""
+TODO
+"""
 function fmi3IsScheduledExecution(fmu::FMU3)
     fmi3IsScheduledExecution(fmu.modelDescription)
 end
