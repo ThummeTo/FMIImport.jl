916e78ca55633f48fac90a0d77b056b10ddcabae
ThummeTo <83663542+ThummeTo@users.noreply.github.com>
2026-04-30 14:06:23 +0200

    add LSSA import (#143) (#145)
    * add lssa import (#143)
    
    * version inc, formatting
    
    * fixed tests
    
    * format
    
    * removing comments
    
    * added licenseing
    
    * LSSA tests for windows only
    
    * code formatting
    
    ---------
    
    Co-authored-by: lilliVanillli <lilli.schmiderer@gmail.com>
    Co-authored-by: ThummeTo <tobias.thummerer@uni-a.de>

--- src/FMI2/ext.jl
+++ src/FMI2/ext.jl
@@ -27,8 +27,8 @@ Retrieves all the pointers of binary functions.
 function createFMU2(
     fmuPath,
     fmuZipPath;
-    type::Union{Symbol,fmi2Type,Nothing} = nothing,
-    logLevel::Union{FMULogLevel,Symbol} = FMULogLevelWarn,
+    type::Union{Symbol,fmi2Type,Nothing}=nothing,
+    logLevel::Union{FMULogLevel,Symbol}=FMULogLevelWarn,
 )
     # Create uninitialized FMU
 
@@ -89,7 +89,7 @@ function createFMU2(
         end
     end
 
-    fmuName = getModelIdentifier(fmu.modelDescription; type = fmu.type) # tmpName[length(tmpName)]
+    fmuName = getModelIdentifier(fmu.modelDescription; type=fmu.type) # tmpName[length(tmpName)]
 
     directoryBinary = ""
     pathToBinary = ""
